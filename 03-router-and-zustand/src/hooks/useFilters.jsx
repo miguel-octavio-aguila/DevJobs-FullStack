1aec2cc418ace1bf12c3f47543c31f2ce688fd8e
miguel-octavio-aguila <migueloctavioaguila1@gmail.com>
2025-11-22 04:19:50 -0600

    feat: add `useFilters` hook for managing job search, filtering, and pagination with URL search parameters.

--- 03-router-and-zustand/src/hooks/useFilters.jsx
+++ 03-router-and-zustand/src/hooks/useFilters.jsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "./useRouter.jsx"
 
+import { useSearchParams } from "react-router";
+
 const RESULTS_PER_PAGE = 5
 
 export function useFilters() {
+    const [searchParams, setSearchParams] = useSearchParams()
+    let params = searchParams
+
     const [filters, setFilters] = useState(() => {
-        const params = new URLSearchParams(window.location.search)
+        // we get the filters from the search params so we don't need to use the window.location.search
+        // const params = new URLSearchParams(window.location.search)
         return {
             technology: params.get('technology') || '',
             location: params.get('type') || '',
@@ -14,11 +20,13 @@ export function useFilters() {
     })
 
     const [textFilter, setTextFilter] = useState(() => {
-        const params = new URLSearchParams(window.location.search)
-        return params.get('text') || ''
+        // we get the text filter from the search params so we don't need to use the window.location.search
+        // const params = new URLSearchParams(window.location.search)
+        params.get('text') || ''
     })
     const [currentPage, setCurrentPage] = useState(() => {
-        const params = new URLSearchParams(window.location.search)
+        // we get the current page from the search params so we don't need to use the window.location.search
+        // const params = new URLSearchParams(window.location.search)
         const page = Number(params.get('page'))
         return !page || page < 1 ? 1 : page
     })
@@ -72,21 +80,28 @@ export function useFilters() {
     }, [textFilter, filters, currentPage])
 
     useEffect(() => {
-        const params = new URLSearchParams()
+        // we get the params from the search params so we don't need to use the window.location.search
+        //const params = new URLSearchParams()
+        setSearchParams((params) => {
+            // we don't use .append anymore because we are using the router
+            // we use .set because we want to replace the existing params
+            if(filters.technology) params.set('technology', filters.technology)
+            if(filters.location) params.set('type', filters.location)
+            if(filters.experienceLevel) params.set('level', filters.experienceLevel)
+            if(textFilter) params.set('text', textFilter)
+    
+            if(currentPage > 1) params.set('page', currentPage)
+    
+            return params
+            // we don't need to navigate to the new url because we are using the router
+            // const newUrl = params.toString()
+            //     ? `${window.location.pathname}?${params.toString()}`
+            //     : window.location.pathname
+    
+            // navigateTo(newUrl)
+        })
 
-        if(filters.technology) params.append('technology', filters.technology)
-        if(filters.location) params.append('type', filters.location)
-        if(filters.experienceLevel) params.append('level', filters.experienceLevel)
-        if(textFilter) params.append('text', textFilter)
-
-        if(currentPage > 1) params.append('page', currentPage)
-
-        const newUrl = params.toString()
-            ? `${window.location.pathname}?${params.toString()}`
-            : window.location.pathname
-
-        navigateTo(newUrl)
-    }, [currentPage, filters, textFilter, navigateTo])
+    }, [currentPage, filters, textFilter, navigateTo, setSearchParams])
     
     // we calculate the total number of pages
     const totalPages = Math.ceil(total / RESULTS_PER_PAGE) // 10 / 5 = 2.2 = 3 pages
